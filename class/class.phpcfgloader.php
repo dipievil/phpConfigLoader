cfa7d3c3e339808a7ac15144410ea7f4eeac261b
dipievil <dipi.evil@gmail.com>
2015-08-31 12:02:42 -0300

    Ajustes e configurações

--- class/class.phpcfgloader.php
+++ class/class.phpcfgloader.php
@@ -13,9 +13,20 @@ class cfgLoader{
     private $iniFile = "";
     private $fullPath = "";
     private $arrayFile = "";
+    private $internalData = "";
 
-    function cfgLoader($loadType, $jsonFile, $iniFile, $fullPath, $arrayFile){
-        $this->loadType = ($loadType==null) ? "FULL" : $loadType;
+    /**
+     *
+     * Loads the config with optional parameters
+     *
+     * @param null $loadType Try to Loads all files or specific: JSON, INI OR ARRAY
+     * @param null $jsonFile Name of the Json file
+     * @param null $iniFile Name of the Ini file
+     * @param null $fullPath Path to get the cfg files
+     * @param null $arrayFile Name of the array file with $arCfg
+     */
+    function cfgLoader($loadType = null, $jsonFile = null, $iniFile = null , $fullPath = null, $arrayFile = null){
+        $this->loadType = ($loadType==null) ? "ANY" : $loadType;
 
         //Default file names or provided names
         $this->jsonFile = ($jsonFile==null) ? "config.json" : $jsonFile;
@@ -24,34 +35,93 @@ class cfgLoader{
 
         //Default folder for cfg files
         $this->fullPath = ($fullPath == null) ? "../cfg/" : $fullPath;
+
+        switch ($this->loadType){
+            case "ANY":
+                $this->loadJson();
+                $this->loadIni();
+                $this->loadArray();
+                break;
+
+            case "JSON":
+                $this->loadJson();
+                break;
+
+            case "INI":
+                $this->loadIni();
+                break;
+
+            case "ARRAY":
+                $this->loadArray();
+                break;
+        }
     }
 
+    /**
+     * Load parameters from .json
+     */
     private function loadJson(){
-
+        if(file_exists($this->fullPath.$this->jsonFile)) {
+            $arCfg = json_decode(file_get_contents($this->fullPath.$this->iniFile));
+            $this->loadConfigs($arCfg);
+        }
     }
 
+    /**
+     * Load parameters from .ini file
+     */
     private function loadIni(){
-
+        if(file_exists($this->fullPath.$this->iniFile)) {
+            $arCfg = parse_ini_file($this->fullPath.$this->iniFile);
+            $this->loadConfigs($arCfg);
+        }
     }
 
-    private function loadArray($arCfg){
-        $fileArray = $this->fullPath.$this->arrayFile;
-        if(file_exists($fileArray)){
+
+    /**
+     * Load values from default array
+     */
+    private function loadArray(){
+        if(file_exists($this->fullPath.$this->arrayFile)){
             include_once($this->fullPath.$this->arrayFile);
-            if(!empty($arCfg)) {
-                $arKeys = array_keys($arCfg);
-                for($i=0;$i<count($arCfg);$i++){
-                    $this->__set($arKeys[0],$arCfg[0]);
-                }
+            if(!empty($arCfg)){
+                $this->loadConfigs($arCfg);
             }
         }
     }
 
+    /**
+     *
+     * Load parameters from array
+     * @param $arConfig array with parameters
+     */
+    private function loadConfigs($arConfig){
+        print_r($arConfig);
+        if(!empty($arConfig)) {
+            $arKeys = array_keys($arConfig);
+            for($i=0;$i<count($arConfig);$i++){
+                $this->__set($arKeys[$i],$arConfig[$arKeys[$i]]);
+            }
+        }
+    }
+
+    /**
+     *
+     * Dynamic getter
+     * @param $setting string with parameter name
+     */
     public function __get($setting) {
         $this->internalData[$setting];
     }
+
+    /**
+     *
+     * Dynamic setter
+     *
+     * @param $setting  string with parameter name
+     * @param $value string with parameter value
+     */
     public function __set($setting, $value) {
         $this->internalData[$setting] = $value;
     }
-
 }
